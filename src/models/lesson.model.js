9316725948a23cf1878c02ee7f3195b7cd9c900a
Alex <alexandrnezh@gmail.com>
2020-07-08 14:58:36 +0300

    refactor: Modify schema types
    Change fields' 'start_time' and 'end_time' types to String

--- src/models/lesson.model.js
+++ src/models/lesson.model.js
@@ -2,14 +2,25 @@ const mongoose = require('../db/connection');
 
 const { Schema } = mongoose;
 
-const lessonSchema = new Schema({
-  topic: { type: mongoose.SchemaTypes.String, required: true },
-  start_time: { type: mongoose.SchemaTypes.Date, required: true },
-  end_time: { type: mongoose.SchemaTypes.Date, required: true },
-  _teacher_id: { type: mongoose.SchemaTypes.ObjectId, ref: 'teacher' },
-  _group_id: { type: mongoose.SchemaTypes.ObjectId, ref: 'group' },
-  _classroom_id: { type: mongoose.SchemaTypes.ObjectId, ref: 'classroom' },
-});
+const lessonSchema = new Schema(
+  {
+    topic: { type: mongoose.SchemaTypes.String, required: true },
+    start_time: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+      validate: /^\d\d:\d\d$/,
+    },
+    end_time: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+      validate: /^\d\d:\d\d$/,
+    },
+    _teacher_id: { type: mongoose.SchemaTypes.ObjectId, ref: 'teacher' },
+    _group_id: { type: mongoose.SchemaTypes.ObjectId, ref: 'group' },
+    _classroom_id: { type: mongoose.SchemaTypes.ObjectId, ref: 'classroom' },
+  },
+  { versionKey: false },
+);
 
 const collectionName = 'lesson';
 const Lesson = mongoose.model(collectionName, lessonSchema);
